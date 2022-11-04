42f9bc6fda64e49d9e8f03e866b0f29d4a74202d
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-11-04 20:46:30 +0530

    try

---
+++ src/Expense/ExpenseItem.js
@@ -0,0 +1,14 @@
+
+
+function Expense() {
+
+    return ( 
+        <div><h2>Expense Items!</h2>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+        </div>
+        )
+}
+
+export default Expense
