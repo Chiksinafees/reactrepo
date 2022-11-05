53e9402e389b92f7ebb35c6ecfe1faf96876767f
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-11-05 11:57:32 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItem from"./Expense/ExpenseItem"
 function App() {
   return (
     <div>
-      <h1>let's start learning react with Nafees</h1>
+      <h1> let's start learning react with Nafees </h1>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- src/Expense/ExpenseItem.js
+++ src/Expense/ExpenseItem.js
@@ -3,10 +3,10 @@
 function Expense() {
 
     return ( 
-        <div><h2>Expense Items list!</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
+        <div><h2> Expense Items list! </h2>
+        <p> Food Rs 10 </p>
+        <p> Petrol Rs 100 </p>
+        <p> Movies Rs 200 </p>
         </div>
         )
 }
