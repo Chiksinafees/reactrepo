1724d9156f7e338544c2b29bad78ebaa9b6c881b
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-11-05 13:50:46 +0530

    react 1st task expense items adding

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ExpenseItem from"./Expense/ExpenseItem"
 function App() {
   return (
     <div>
-      <h1> let's start learning react with Nafees </h1>
-      <ExpenseItem></ExpenseItem>
+       <h1>let's start learning react with Nafees!</h1>
+       <ExpenseItem></ExpenseItem>
     </div>
   );
 }
